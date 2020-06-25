4dffc4f67e9925a6a6d0f3e5f8a5474b7163d886
Clément HERNANDEZ <clement.hernandez@limayrac.fr>
2020-06-25 12:46:20 +0000

    02 03

--- main.py
+++ main.py
@@ -7,23 +7,41 @@ def exempleHello (msg):
 ###### exercice 01
 def makeDico_A7(filename, sep):
    print("Creation d'un dictionnaire a partir du fichier 'dns.txt' avec '80' entrees")
-   
    f = open(filename, "r")
 
    dico = {}
    
-   for i in range(1,80):
+   for i in range(0,80):
      resol = f.readline().split(sep)
-     dico[resol[i]] = resol[i+1].replace("\n","")
-
-   
+     dico[resol[0]] = resol[1].replace("\n","")
+     
    return dico
 
 ###### exercice 02
+def verifUrl_B3(url):
+  decode = url.count(".")
 
+  if decode == 1:
+    str = url.split(".")
+    x, y = str
+
+    if len(y) == 3:
+      return True
+
+    else:
+      print("url mal formee")
+      return False
+
+  else:
+    print("url mal formee")
+    return False
 
 ###### exercice 03
-
+def getTLD_O8(url):
+  if verifUrl_B3("microsoft.com"):
+    return True
+  else
+    return False
 
 ###### exercice 04
 
@@ -47,51 +65,54 @@ def makeDico_A7(filename, sep):
 # Zone 3 ## zone pour les tests des fonctions
 
 def main() :
-	from re import split
-	###### exercice 00 (la fonction est appelee dans cette zone afin de confirmer son fonctionnement)
-	print("exercice 00 #######################")
-	salutations = exempleHello("Michel")
-	print(salutations)
-	print(salutations.split(sep=" "))
+  from re import split
+	###### exercice 00 (la fonction est appelee dans cette zone afin de confirmer son fonctionnement) 
+  print("exercice 00 #######################")
+  salutations = exempleHello("Michel")
+  print(salutations)
+  print(salutations.split(sep=" "))
 
 	###### exercice 01
-	print("exercice 01 #######################")
-	makeDico_A7("dns.txt", ",")
+  print("exercice 01 #######################")
+  resolDns = makeDico_A7("dns.txt", ",")
+  print(resolDns)
 
 	###### exercice 02
-	print("exercice 02 #######################")
-
+  print("exercice 02 #######################")
+  result = verifUrl_B3("microsoft.com")
+  print(result)
 
 	###### exercice 03
-	print("exercice 03 #######################")
+  print("exercice 03 #######################")
+  result = getTLD_O8(url)
 
 
 	###### exercice 04
-	print("exercice 04 #######################")
+  print("exercice 04 #######################")
 
 
 	###### exercice 05
-	print("exercice 05 #######################")
+  print("exercice 05 #######################")
 
 
 	###### exercice 06
-	print("exercice 06 #######################")
+  print("exercice 06 #######################")
 
 	# Zone 4 ## zone pour les tests de la classe
 
 	###### exercice 07
-	print("exercice 07 #######################")
+  print("exercice 07 #######################")
 
 
 	###### exercice 08
-	print("exercice 08 #######################")
+  print("exercice 08 #######################")
 
 
 	###### exercice 09
-	print("exercice 09 #######################")
+  print("exercice 09 #######################")
 	
 	###### exercice 10
-	print("exercice 10 #######################")
+  print("exercice 10 #######################")
 
 if __name__=="__main__":
 	print("main()")
